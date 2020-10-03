d33ba310a2174531d50a50b8a5de1096f102278a
Stefan Petersen <stefangp88@gmail.com>
2020-10-03 18:33:37 +0200

    removed unnessary usings

--- IdentityServerAddOn/IdentityServer/Data/AppDbContext.cs
+++ IdentityServerAddOn/IdentityServer/Data/AppDbContext.cs
@@ -1,7 +1,5 @@
-﻿using System.Reflection;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 
 namespace IdentityServer.Data
 {
