cfefbea9a0c06bb609f5832c203ed9c8ac122a72
Stefan Petersen <stefangp88@gmail.com>
2020-10-10 22:55:19 +0200

    added dipose pattern

--- IdentityServerAddOn/Ids.SimpleAdmin.Tests/IdentityFixture.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Tests/IdentityFixture.cs
@@ -1,14 +1,51 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Ids.SimpleAdmin.Tests
 {
-    public class IdentityFixture
+    public class IdentityFixture: IDisposable
     {
+        private bool disposedValue;
+
         private DbContext dbContext { get; set; }
 
         private IServiceCollection Services { get; set; }
 
         private ServiceProvider ServiceProvider { get; set; }
+
+
+
+        #region dispose
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~IdentityFixture()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion dispose
     }
 }
--- IdentityServerAddOn/Ids.SimpleAdmin.Tests/IdentityRoleTests.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Tests/IdentityRoleTests.cs
@@ -1,8 +1,47 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Ids.SimpleAdmin.Tests
 {
-    public class IdentityRoleTests : IClassFixture<IdentityFixture>
+    public class IdentityRoleTests : IClassFixture<IdentityFixture>, IDisposable
     {
+
+
+
+
+        #region dispose
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~IdentityRoleTests()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion dispose
     }
 }
