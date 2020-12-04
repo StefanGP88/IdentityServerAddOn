46e9347e3052df57ce97120aa6fa09fa8d9243ea
Stefan Petersen <stefangp88@gmail.com>
2020-12-04 22:07:49 +0100

    genericfy some stuff

--- IdentityServerAddOn/RazorTestLibrary/Areas/SimpleAdmin/Pages/BasePageModel.cs
+++ IdentityServerAddOn/RazorTestLibrary/Areas/SimpleAdmin/Pages/BasePageModel.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ids.SimpleAdmin.Backend.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorTestLibrary.Areas.SimpleAdmin.Pages
 {
-    public class BasePageModel : PageModel
+    public class BasePageModel<T> : PageModel
     {
         [FromQuery(Name = "pagesize")]
         public int PageSize { get; set; }
 
         [FromQuery(Name = "pagenumber")]
         public int PageNumber { get; set; }
+
+        public ListDto<T> List { get; set; }
     }
 }
--- IdentityServerAddOn/RazorTestLibrary/Areas/SimpleAdmin/Pages/Roles/All.cshtml.cs
+++ IdentityServerAddOn/RazorTestLibrary/Areas/SimpleAdmin/Pages/Roles/All.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading;
 
 namespace RazorTestLibrary.Areas.SimpleAdmin.Pages.Roles
 {
-    public class AllModel : BasePageModel
+    public class AllModel : BasePageModel<RoleResponseDto>
     {
         private readonly IRoleHandler _handler;
         public AllModel(IRoleHandler handler)
@@ -13,28 +13,26 @@ namespace RazorTestLibrary.Areas.SimpleAdmin.Pages.Roles
             _handler = handler;
         }
 
-        public ListDto<RoleResponseDto> List { get; set; }
-
-        public IActionResult OnGet( CancellationToken cancel = default)
+        public IActionResult OnGet(CancellationToken cancel = default)
         {
             List = _handler.ReadAllRoles(PageNumber, PageSize, cancel).GetAwaiter().GetResult();
             return Page();
         }
-        public IActionResult OnPostAdd([FromForm] string roleName,  CancellationToken cancel = default)
+        public IActionResult OnPostAdd([FromForm] string roleName, CancellationToken cancel = default)
         {
             _ = _handler.CreateRole(roleName).GetAwaiter().GetResult();
             List = _handler.ReadAllRoles(PageNumber, PageSize, cancel).GetAwaiter().GetResult();
             return Page();
         }
 
-        public IActionResult OnPostEdit(UpdateRoleRequestDto dto, CancellationToken cancel = default)
+        public IActionResult OnPostEdit([FromForm] UpdateRoleRequestDto dto, CancellationToken cancel = default)
         {
             _ = _handler.UpdateRole(dto).GetAwaiter().GetResult();
             List = _handler.ReadAllRoles(PageNumber, PageSize, cancel).GetAwaiter().GetResult();
             return Page();
         }
 
-        public IActionResult OnPostDelete(DeleteRoleRequestDto dto,  CancellationToken cancel = default)
+        public IActionResult OnPostDelete([FromForm] DeleteRoleRequestDto dto, CancellationToken cancel = default)
         {
             _handler.DeleteRole(dto).GetAwaiter().GetResult();
             List = _handler.ReadAllRoles(PageNumber, PageSize, cancel).GetAwaiter().GetResult();
