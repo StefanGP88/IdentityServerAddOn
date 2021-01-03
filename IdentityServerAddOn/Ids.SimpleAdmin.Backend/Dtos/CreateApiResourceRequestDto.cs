2409cfc81a369c07531e20619c92703f60cbd032
Stefan Petersen <stefangp88@gmail.com>
2021-01-03 01:41:29 +0100

    started api resource

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Dtos/CreateApiResourceRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ids.SimpleAdmin.Backend.Dtos
+{
+    public class CreateApiResourceRequestDto
+    {
+        public bool Enabled { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public string AllowedAccessTokenSigningAlgorithmes { get; set; }
+        public bool ShowInDiscoveryDocument { get; set; }
+        public DateTime Created { get; set; }
+        public bool NonEditable { get; set; }
+    }
+}
---
+++ IdentityServerAddOn/RazorTestLibrary/Areas/SimpleAdmin/Pages/ApiResources.cshtml.cs
@@ -0,0 +1,28 @@
+using Ids.SimpleAdmin.Backend.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace RazorTestLibrary.Areas.SimpleAdmin.Pages
+{
+    public class ApiResourcesModel : BasePageModel<ApiResourceResponseDto>
+    {
+        public IActionResult OnGet(CancellationToken cancel = default)
+        {
+            return Page();
+        }
+        public IActionResult OnPostAdd([FromForm] CreateApiScopeRequestDto dto, CancellationToken cancel = default)
+        {
+            return Page();
+        }
+
+        public IActionResult OnPostEdit([FromForm] UpdateApiScopeRequestDto dto, CancellationToken cancel = default)
+        {
+            return Page();
+        }
+
+        public IActionResult OnPostDelete([FromForm] string id, CancellationToken cancel = default)
+        {
+            return Page();
+        }
+    }
+}
