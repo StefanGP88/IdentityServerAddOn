aa0c4afb5a4df88064e1ea00992004d97d268651
Stefan Petersen <stefangp88@gmail.com>
2021-02-02 22:00:19 +0100

    secrettablerow

--- IdentityServerAddOn/RazorTestLibrary/TempViewModels.cs
+++ IdentityServerAddOn/RazorTestLibrary/TempViewModels.cs
@@ -1,8 +1,19 @@
-﻿namespace RazorTestLibrary
+﻿using System;
+
+namespace RazorTestLibrary
 {
     public class PropertRowModel
     {
         public string Property { get; set; }
         public string PropertyKey { get; set; }
+    }
+
+    public class SecretRowModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public DateTime Expiration { get; set; }
+        public DateTime Created { get; set; }
+        public string Description { get; set; }
     }
 }
