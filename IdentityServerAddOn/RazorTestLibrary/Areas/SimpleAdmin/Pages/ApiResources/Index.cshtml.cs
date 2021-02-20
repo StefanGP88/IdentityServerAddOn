db4e8785592d63c835226f220d901597bf8c2f66
Stefan Petersen <stefangp88@gmail.com>
2021-02-20 15:53:16 +0100

    row loop

--- IdentityServerAddOn/RazorTestLibrary/Areas/SimpleAdmin/Pages/ApiResources/Index.cshtml.cs
+++ IdentityServerAddOn/RazorTestLibrary/Areas/SimpleAdmin/Pages/ApiResources/Index.cshtml.cs
@@ -1,11 +1,18 @@
 using Ids.SimpleAdmin.Backend.Dtos;
+using Ids.SimpleAdmin.Backend.Handlers.Interfaces;
 
 namespace RazorTestLibrary.Areas.SimpleAdmin.Pages.ApiResources
 {
     public class IndexModel : BaseIndexPage<ApiResourceResponseDto>
     {
+        private readonly IApiResourceHandler _hander;
+        public IndexModel(IApiResourceHandler apiResourceHandler)
+        {
+            _hander = apiResourceHandler;
+        }
         public void OnGet()
         {
+            List = _hander.ReadAll(PageNumber, PageSize, default).GetAwaiter().GetResult();
         }
     }
 }
