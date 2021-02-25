c67ff335224ee4d550671d6147f0908d1d4eba8e
Stefan Petersen <stefangp88@gmail.com>
2021-02-25 18:07:18 +0100

    old files moved

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/MyFeature/Pages/BasePageModel.cs
@@ -0,0 +1,20 @@
+﻿using Ids.SimpleAdmin.Backend.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages
+{
+    public class BasePageModel<TResource, TProperties> : PageModel 
+    {
+        [FromQuery(Name = "pagesize")]
+        public int PageSize { get; set; }
+
+        [FromQuery(Name = "pagenumber")]
+        public int PageNumber { get; set; }
+
+        public ListDto<TResource> List { get; set; }
+        public static TProperties Properties { get; } = (TProperties)Activator.CreateInstance(typeof(TProperties));
+
+    }
+}
