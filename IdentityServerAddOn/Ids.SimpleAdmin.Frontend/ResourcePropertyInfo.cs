8d7e8d1968e7bb35c8bcfddb7cbc04004f0696fb
Stefan Petersen <stefangp88@gmail.com>
2021-02-26 20:35:27 +0100

    some resource property stuff

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/ResourcePropertyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Ids.SimpleAdmin.Frontend
+{
+    public class ResourcePropertyInfo
+    {
+        public string Name { get; set; }
+        public List<string> Columns { get; set; }
+        public string FormPartialName { get; set; }
+    }
+}
