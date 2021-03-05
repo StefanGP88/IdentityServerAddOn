01d8cfc792a93095662489451816302f1de5f9ad
Stefan Petersen <stefangp88@gmail.com>
2021-03-05 23:09:02 +0100

    renamed info model

--- IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/ApiResources/Info.cshtml.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/ApiResources/Info.cshtml.cs
@@ -4,9 +4,9 @@ using Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Shared;
 
 namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.ApiResources
 {
-    public class EditModel : BaseInfoPage<ApiResourceContract, int?>
+    public class InfoModel : BaseInfoPage<ApiResourceContract, int?>
     {
-        public EditModel(IHandler<ApiResourceContract, int?> handler) : base(handler) { }
+        public InfoModel(IHandler<ApiResourceContract, int?> handler) : base(handler) { }
 
     }
 }
