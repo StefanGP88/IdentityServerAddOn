eb281c3c99a069c6b9c85a07a24082f47f019ec6
Stefan Petersen <stefangp88@gmail.com>
2021-03-07 16:38:18 +0100

    implemented resource handler

--- IdentityServerAddOn/Ids.SimpleAdmin.Backend/Handlers/ApiResourceHandler.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Handlers/ApiResourceHandler.cs
@@ -1,9 +1,9 @@
 ﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Entities;
 using Ids.SimpleAdmin.Backend.Handlers.Interfaces;
 using Ids.SimpleAdmin.Contracts;
 using Mapster;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -42,24 +42,57 @@ namespace Ids.SimpleAdmin.Backend.Handlers
             };
         }
 
-        public async Task<ListDto<ApiResourceContract>> Delete(int? id, int page, int pageSize, CancellationToken cancel)
+        public async Task<ApiResourceContract> Get(int? id, CancellationToken cancel)
         {
-            return null;
+            return await _confContext.ApiResources
+                .AsNoTracking()
+                .Where(x => x.Id == id)
+                .Include(x => x.UserClaims)
+                .Include(x => x.Properties)
+                .Include(x => x.Scopes)
+                .Include(x => x.Secrets)
+                .ProjectToType<ApiResourceContract>()
+                .FirstOrDefaultAsync(cancel)
+                .ConfigureAwait(false);
         }
 
-        public async Task<ApiResourceContract> Create(ApiResourceContract dto, int page, int pageSize, CancellationToken cancel)
+        public async Task Delete(int? id, CancellationToken cancel)
         {
-            return null;
+            var resource = await _confContext.ApiResources
+                .Where(x => x.Id == id)
+                .FirstAsync(cancel)
+                .ConfigureAwait(false);
+
+            _confContext.ApiResources
+                .Remove(resource);
+
+            await _confContext.SaveChangesAsync(cancel).ConfigureAwait(false);
         }
 
-        public async Task<ApiResourceContract> Get(int? id, int page, int pageSize, CancellationToken cancel)
+        public async Task<ApiResourceContract> Create(ApiResourceContract dto, CancellationToken cancel)
         {
-            return null;
+            var model = dto.Adapt<ApiResource>();
+            await _confContext.ApiResources.AddAsync(model, cancel).ConfigureAwait(false);
+            await _confContext.SaveChangesAsync(cancel).ConfigureAwait(false);
+            return model.Adapt<ApiResourceContract>();
         }
 
-        public async Task<ApiResourceContract> Update(ApiResourceContract dto, int page, int pageSize, CancellationToken cancel)
+        public async Task<ApiResourceContract> Update(ApiResourceContract dto, CancellationToken cancel)
         {
-            return null;
+            var model = await _confContext.ApiResources
+                .AsNoTracking()
+                .Where(x => x.Id == dto.Id)
+                .Include(x => x.UserClaims)
+                .Include(x => x.Properties)
+                .Include(x => x.Scopes)
+                .Include(x => x.Secrets)
+                .FirstOrDefaultAsync(cancel)
+                .ConfigureAwait(false);
+
+            dto.Adapt(model);
+            _confContext.ApiResources.Update(model);
+            await _confContext.SaveChangesAsync(cancel);
+            return model.Adapt<ApiResourceContract>();
         }
     }
 }
--- IdentityServerAddOn/Ids.SimpleAdmin.Backend/Handlers/Interfaces/IHandler.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Handlers/Interfaces/IHandler.cs
@@ -7,9 +7,9 @@ namespace Ids.SimpleAdmin.Backend.Handlers.Interfaces
     public interface IHandler<TData, TIdentifier>
     {
         Task<ListDto<TData>> GetAll(int page, int pageSize, CancellationToken cancel);
-        Task<TData> Get(TIdentifier id, int page, int pageSize, CancellationToken cancel);
-        Task<ListDto<TData>> Delete(TIdentifier id, int page, int pageSize, CancellationToken cancel);
-        Task<TData> Create(TData dto, int page, int pageSize, CancellationToken cancel);
-        Task<TData> Update(TData dto, int page, int pageSize, CancellationToken cancel);
+        Task<TData> Get(TIdentifier id, CancellationToken cancel);
+        Task Delete(TIdentifier id, CancellationToken cancel);
+        Task<TData> Create(TData dto, CancellationToken cancel);
+        Task<TData> Update(TData dto, CancellationToken cancel);
     }
 }
