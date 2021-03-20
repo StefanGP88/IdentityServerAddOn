d88fca15b072d5d6e77797d6e36ecfa86b4f7155
Stefan Petersen <stefangp88@gmail.com>
2021-03-20 23:06:16 +0100

    indentiy resource index

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/IdentityResources/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.IdentityResources
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
