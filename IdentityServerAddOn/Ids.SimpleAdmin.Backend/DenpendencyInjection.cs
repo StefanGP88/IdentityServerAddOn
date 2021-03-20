4296991ca051afd931dc6583c05c606cd76f859b
Stefan Petersen <stefangp88@gmail.com>
2021-03-20 21:02:24 +0100

    added api scope validator

--- IdentityServerAddOn/Ids.SimpleAdmin.Backend/DenpendencyInjection.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/DenpendencyInjection.cs
@@ -2,6 +2,8 @@
 using Ids.SimpleAdmin.Backend.Handlers.Interfaces;
 using Ids.SimpleAdmin.Backend.Handlers;
 using Ids.SimpleAdmin.Contracts;
+using FluentValidation;
+using Ids.SimpleAdmin.Backend.Validators;
 
 namespace Ids.SimpleAdmin.Backend
 {
@@ -9,8 +11,19 @@ namespace Ids.SimpleAdmin.Backend
     {
         public static void AddIdentityServerAddOn(this IServiceCollection sc)
         {
-            //TODO: make scoped
+            //handlers
             sc.AddScoped<IHandler<ApiResourceContract, int?>, ApiResourceHandler>();
+
+            //validators
+            sc.AddTransient<IValidator<ApiResourceContract>, ApiResourceValidator>();
+            sc.AddTransient<IValidator<ApiResourceClaimsContract>, ApiResourceClaimsValidator>();
+            sc.AddTransient<IValidator<ApiResourceScopesContract>, ApiResourceScopesValidator>();
+            sc.AddTransient<IValidator<ApiResourceSecretsContract>, ApiResourceSecretsValidator>();
+            sc.AddTransient<IValidator<ApiResourcePropertiesContract>, ApiResourcePropertiesValidator>();
+            sc.AddTransient<IValidator<ApiScopeContract>, ApiScopeValidator>();
+            sc.AddTransient<IValidator<ApiScopeClaimsContract>, ApiScopeClaimsValidator>();
+            sc.AddTransient<IValidator<ApiScopePropertiesContract>, ApiScopePropertiesValidator>();
+            sc.AddTransient<ValidationFactory>();
         }
     }
 }
---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/ApiScopeValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Ids.SimpleAdmin.Contracts;
+
+namespace Ids.SimpleAdmin.Backend.Validators
+{
+    public class ApiScopeValidator : AbstractValidator<ApiScopeContract>
+    {
+        public ApiScopeValidator()
+        {
+            RuleFor(x => x.Enabled).NotNull();
+            RuleFor(x => x.Name).MaximumLength(200).NotNull();
+            RuleFor(x => x.DisplayName).MaximumLength(200);
+            RuleFor(x => x.Description).MaximumLength(1000);
+            RuleFor(x => x.Required).NotNull();
+            RuleFor(x => x.Emphasize).NotNull();
+            RuleFor(x => x.ShowInDiscoveryDocument).NotNull();
+            RuleForEach(x => x.Claims).SetValidator(new ApiScopeClaimsValidator());
+            RuleForEach(x => x.Properties).SetValidator(new ApiScopePropertiesValidator());
+        }
+    }
+    public class ApiScopeClaimsValidator : AbstractValidator<ApiScopeClaimsContract>
+    {
+        public ApiScopeClaimsValidator()
+        {
+            RuleFor(x => x.Type).MaximumLength(200).NotNull();
+        }
+    }
+    public class ApiScopePropertiesValidator : AbstractValidator<ApiScopePropertiesContract>
+    {
+        public ApiScopePropertiesValidator()
+        {
+            RuleFor(x => x.Key).MaximumLength(250).NotNull();
+            RuleFor(x => x.Value).MaximumLength(1000).NotNull();
+        }
+    }
+}
