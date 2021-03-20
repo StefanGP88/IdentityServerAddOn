0b5c43bf0015e5991c49965947fc230029d1f591
Stefan Petersen <stefangp88@gmail.com>
2021-03-20 23:13:58 +0100

    identity resources info

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/IdentityResources/Info.cshtml.cs
@@ -0,0 +1,22 @@
+using Ids.SimpleAdmin.Backend.Handlers.Interfaces;
+using Ids.SimpleAdmin.Contracts;
+using Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.IdentityResources
+{
+    public class InfoModel : BaseInfoPage<IdentityResourceContract, int?>
+    {
+        public InfoModel(IHandler<IdentityResourceContract, int?> handler) : base(handler) { }
+
+        public PartialViewResult OnGetUserclaims(ApiScopeClaimsContract model)
+        {
+            return OnGetPartial("Userclaims", model);
+        }
+        public PartialViewResult OnGetProperties(ApiScopePropertiesContract model)
+        {
+            return OnGetPartial("Properties", model);
+        }
+    }
+}
+}
