e3e602164596a114a5d8b58d291d33c656d4dc26
Stefan Petersen <stefangp88@gmail.com>
2021-03-20 21:45:41 +0100

    implemented info

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/ApiScope/Info.cshtml.cs
@@ -0,0 +1,21 @@
+using Ids.SimpleAdmin.Backend.Handlers.Interfaces;
+using Ids.SimpleAdmin.Contracts;
+using Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.ApiScope
+{
+    public class InfoModel : BaseInfoPage<ApiScopeContract, int?>
+    {
+        public InfoModel(IHandler<ApiScopeContract, int?> handler) : base(handler) { }
+
+        public PartialViewResult OnGetUserclaims(ApiResourceClaimsContract model)
+        {
+            return OnGetPartial("Userclaims", model);
+        }
+        public PartialViewResult OnGetProperties(ApiResourcePropertiesContract model)
+        {
+            return OnGetPartial("Properties", model);
+        }
+    }
+}
