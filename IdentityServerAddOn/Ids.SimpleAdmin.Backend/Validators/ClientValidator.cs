160fef6bf1dee8c6c67bd970d2841b397c357316
Stefan Petersen <stefangp88@gmail.com>
2021-03-21 01:17:41 +0100

    started client validator

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/ClientValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Ids.SimpleAdmin.Contracts;
+
+namespace Ids.SimpleAdmin.Backend.Validators
+{
+    public class ClientValidator: AbstractValidator<ClientsContract>
+    {
+      public  ClientValidator()
+        {
+
+        }
+    }
+}
--- IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/IdentityResourceValidator.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/IdentityResourceValidator.cs
@@ -1,10 +1,5 @@
 ﻿using FluentValidation;
 using Ids.SimpleAdmin.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ids.SimpleAdmin.Backend.Validators
 {
