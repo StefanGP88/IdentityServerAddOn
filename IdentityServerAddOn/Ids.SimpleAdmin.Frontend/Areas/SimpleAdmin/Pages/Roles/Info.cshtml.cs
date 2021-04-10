8ab28154ef99f2964483139b1d6e1ca52069a2f6
Stefan Petersen <stefangp88@gmail.com>
2021-04-10 15:32:45 +0200

    role claim partial

--- IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/Roles/Info.cshtml.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/Roles/Info.cshtml.cs
@@ -1,11 +1,16 @@
 using Ids.SimpleAdmin.Backend.Handlers.Interfaces;
 using Ids.SimpleAdmin.Contracts;
 using Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Roles
 {
     public class InfoModel : BaseInfoPage<RolesContract, string>
     {
         public InfoModel(IHandler<RolesContract, string> handler) : base(handler) { }
+        public PartialViewResult OnGetRoleClaims(RoleClaimsContract model)
+        {
+            return OnGetPartial("RoleClaims", model);
+        }
     }
 }
