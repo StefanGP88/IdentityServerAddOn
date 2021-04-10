16780b7c2d2a51d374c6087e076d1953ce0be78d
Stefan Petersen <stefangp88@gmail.com>
2021-04-10 20:34:11 +0200

    temp fix IdentityDbContext dependency injection

--- IdentityServerAddOn/Ids.SimpleAdmin.Frontend/DependencyInjection.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/DependencyInjection.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation.AspNetCore;
 using Ids.SimpleAdmin.Backend;
-using System.Reflection;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using FluentValidation.AspNetCore;
-using FluentValidation;
-using Ids.SimpleAdmin.Backend.Validators;
-using Ids.SimpleAdmin.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ids.SimpleAdmin.Frontend
 {
@@ -18,6 +14,9 @@ namespace Ids.SimpleAdmin.Frontend
             services.AddScoped<PageSizeMiddleware>();
 
             services.AddHttpContextAccessor();
+
+
+
         }
         public static IMvcBuilder AddRazorPagesForSimpleAdmin(this IMvcBuilder builder)
         {
@@ -25,6 +24,43 @@ namespace Ids.SimpleAdmin.Frontend
             builder.AddFluentValidation();
             var assembly = Assembly.GetExecutingAssembly().GetName().Name;
             builder.AddApplicationPart(Assembly.Load(assembly));
+
+            /*************************/
+            //TODO: MOVE TO SOMEWHERE MORE SENSIBLE
+            //TODO TODO REWRITE TO NOT KNOW APPDBCONTEXT NAME
+            var entryAsm = Assembly.GetEntryAssembly(); //can this be called from anywhere and still get Identityserver project
+            //var callingAssembly = Assembly.GetCallingAssembly();
+
+            var scEnumirator = builder.Services.GetEnumerator();
+            while (scEnumirator.MoveNext())
+            {
+                var current = scEnumirator.Current;
+                var serviceType = current.ServiceType;
+
+                if (serviceType.BaseType is not null)
+                {
+                    if (serviceType.Name == "AppDbContext")
+                    {
+                        //var appDbCtx = callingAssembly.GetType(serviceType.FullName);
+                        var appDbCtx = entryAsm.GetType(serviceType.FullName);
+                        System.Console.WriteLine(appDbCtx);
+                        var baseDbCtx = appDbCtx.BaseType;
+
+                        builder.Services.AddScoped(baseDbCtx, appDbCtx);
+
+                        var abc = 2; //this is where we can find adcontext type
+                        break;
+                    }
+                    else
+                    {
+                        System.Console.WriteLine(serviceType.Name);
+                    }
+                }
+
+
+            }
+            /*********************************************/
+
 
             return builder;
         }
