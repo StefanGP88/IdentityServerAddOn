f8110c696c907a6e1fc69262a6033c80bb3bfab8
Stefan Petersen <stefangp88@gmail.com>
2021-04-14 20:29:38 +0200

    added contract

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Contracts/UserContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ids.SimpleAdmin.Contracts
+{
+    public class UserContract : Identifyable<string>
+    {
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string ReplacePassword { get; set; }
+        public string ConcurrencyStampo { get; set; }
+        public string  PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public DateTime LockoutEnd { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public bool ResetAccessFailedCount { get; set; }
+    }
+}
