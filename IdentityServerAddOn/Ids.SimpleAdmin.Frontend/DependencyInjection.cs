8fede9f6c28e338586f0d791e5ddf3f9f0e37440
Stefan Petersen <stefangp88@gmail.com>
2021-04-30 13:12:28 +0200

    refactored Identity db context injection

--- IdentityServerAddOn/Ids.SimpleAdmin.Frontend/DependencyInjection.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/DependencyInjection.cs
@@ -1,9 +1,7 @@
 ﻿using FluentValidation.AspNetCore;
 using Ids.SimpleAdmin.Backend;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Reflection;
@@ -16,61 +14,38 @@ namespace Ids.SimpleAdmin.Frontend
         public static void AddSimpleAdmin(this IMvcBuilder builder)
         {
             builder.Services.AddSimpleAdminBackend();
-            builder.Services.AddSimpleAdminBackend();
             builder.Services.AddScoped<PageSizeMiddleware>();
             builder.Services.AddHttpContextAccessor();
             builder.AddFluentValidation();
             
-            
             var assembly = Assembly.GetExecutingAssembly().GetName().Name;
-            builder.AddApplicationPart(Assembly.Load(assembly));
-            
-            
-            
-            /*************************/
-            //TODO: MOVE TO SOMEWHERE MORE SENSIBLE
-            //TODO TODO REWRITE TO NOT KNOW APPDBCONTEXT NAME
-            
-            var entryAsm = Assembly.GetEntryAssembly(); //can this be called from anywhere and still get Identityserver project
-                                                        //var callingAssembly = Assembly.GetCallingAssembly();
-            
-            var scEnumirator = builder.Services.GetEnumerator();
-            while (scEnumirator.MoveNext())
-            {
-                var current = scEnumirator.Current;
-                var serviceType = current.ServiceType;
-            
-                if (serviceType.BaseType is not null)
-                {
-                    if (serviceType.BaseType.Name == nameof(IdentityDbContext))
-                    {
-                        //var appDbCtx = callingAssembly.GetType(serviceType.FullName);
-                        var appDbCtx = entryAsm.GetType(serviceType.FullName);
-                        //System.Console.WriteLine(appDbCtx);
-                        var baseDbCtx = appDbCtx.BaseType;
-            
-                        builder.Services.AddScoped(baseDbCtx, appDbCtx);
-            
-                        var abc = 2; //this is where we can find adcontext type
-                        break;
-                    }
-                    else
-                    {
-                        //System.Console.WriteLine(serviceType.Name);
-                    }
-                }
-            
-            
-            }
-            
-            /*********************************************/
-            
-            //builder.Services.AddScoped(typeof(DbContext), typeof(IdentityDbContext));
+            builder.AddApplicationPart(Assembly.Load(assembly ?? throw new InvalidOperationException()));
+
+            EnsureIdentityDbContextInjection(builder);
         }
         public static void UseSimpleAdmin(this IApplicationBuilder app)
         {
             app.UseMiddleware<PageSizeMiddleware>();
             app.UseEndpoints(endpoints => endpoints.MapRazorPages());
+        }
+
+        private static void EnsureIdentityDbContextInjection(IMvcBuilder builder)
+        {
+            var entryAsm = Assembly.GetEntryAssembly();
+            using var enumerator = builder.Services.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                var serviceType = current?.ServiceType;
+
+                if (serviceType?.BaseType is null) continue;
+                if (serviceType.BaseType.Name != nameof(IdentityDbContext)) continue;
+                
+                var appDbCtx = entryAsm?.GetType(serviceType.FullName ?? serviceType.Name);
+                var baseDbCtx = appDbCtx?.BaseType;
+                builder.Services.AddScoped(baseDbCtx ?? throw new InvalidOperationException(), appDbCtx);
+                break;
+            }
         }
     }
 }
