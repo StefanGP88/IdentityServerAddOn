f0f9762618703ca781f4dc2cee0a31e1236428d5
Stefan Petersen <stefangp88@gmail.com>
2021-06-15 20:44:28 +0200

    FormSectionComponent poc

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/Shared/Components/FormSection/FormSectionViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Shared.Components.FormSection
+{
+    public class FormSectionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(ComponentData data)
+        {
+            return View(data);
+        }
+    }
+    public class ComponentData
+    {
+        public string Title;
+        public Action Action;
+    }
+}
