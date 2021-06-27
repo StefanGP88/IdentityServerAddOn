c6e3708b5ddd1122dc981d1ea5fae45346a2c87e
Stefan Petersen <stefangp88@gmail.com>
2021-06-27 20:02:03 +0200

    api scope validator extracted

--- IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/ApiScopeValidator.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/ApiScopeValidator.cs
@@ -14,23 +14,8 @@ namespace Ids.SimpleAdmin.Backend.Validators
             RuleFor(x => x.Required).NotNull();
             RuleFor(x => x.Emphasize).NotNull();
             RuleFor(x => x.ShowInDiscoveryDocument).NotNull();
-            RuleForEach(x => x.UserClaims).SetValidator(new ApiScopeClaimsValidator());
-            RuleForEach(x => x.Properties).SetValidator(new ApiScopePropertiesValidator());
-        }
-    }
-    public class ApiScopeClaimsValidator : AbstractValidator<ApiScopeClaimsContract>
-    {
-        public ApiScopeClaimsValidator()
-        {
-            RuleFor(x => x.Type).MaximumLength(200).NotNull();
-        }
-    }
-    public class ApiScopePropertiesValidator : AbstractValidator<ApiScopePropertiesContract>
-    {
-        public ApiScopePropertiesValidator()
-        {
-            RuleFor(x => x.Key).MaximumLength(250).NotNull();
-            RuleFor(x => x.Value).MaximumLength(2000).NotNull();
+            RuleForEach(x => x.UserClaims).SetValidator(new ClaimValidator());
+            RuleForEach(x => x.Properties).SetValidator(new PropertyValidator());
         }
     }
 }
