406ab78fbdff94cff15a6244374ce6509990d20d
Stefan Petersen <stefangp88@gmail.com>
2021-07-03 19:32:28 +0200

    sectionized

--- IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/Clients/Info.cshtml.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/Clients/Info.cshtml.cs
@@ -1,7 +1,6 @@
 using Ids.SimpleAdmin.Backend.Handlers.Interfaces;
 using Ids.SimpleAdmin.Contracts;
 using Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Shared;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Clients
 {
