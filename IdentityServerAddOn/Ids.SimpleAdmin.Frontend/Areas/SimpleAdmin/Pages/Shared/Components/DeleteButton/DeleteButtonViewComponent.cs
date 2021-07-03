4df1da72921e39aa3bf2a4544a8f6d31bf847f40
Stefan Petersen <stefangp88@gmail.com>
2021-07-03 11:35:38 +0200

    identity prodiuvder restrictions

---
+++ IdentityServerAddOn/Ids.SimpleAdmin.Frontend/Areas/SimpleAdmin/Pages/Shared/Components/DeleteButton/DeleteButtonViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ids.SimpleAdmin.Frontend.Areas.SimpleAdmin.Pages.Shared.Components.DeleteButton
+{
+    public class DeleteButtonViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string title, string target)
+        {
+            return View(new ButtonDataData
+            {
+                Title = title,
+                Target = target
+            });
+        }
+    }
+    public class ButtonDataData
+    {
+        public string Title { get; set; }
+        public string Target { get; set; }
+    }
+}
