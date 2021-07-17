23d0759436cc3088b941245c51a6b014db13cc24
Stefan Petersen <stefangp88@gmail.com>
2021-07-17 21:00:57 +0200

    first real unit test

---
+++ IdentityServerAddOn/UnitTests/ContractBuilders/ApiResourceContractBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Ids.SimpleAdmin.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTests.ContractBuilders
+{
+    public class ApiResourceContractBuilder
+    {
+        public ApiResourceContractBuilder()
+        {
+            Default = new ApiResourceContract
+            {
+                AllowedAccessTokenSigningAlgorithms = "",
+                Created = DateTime.UtcNow,
+                Description = "Test api resource description",
+                Claims = new List<ClaimsContract>(),
+                DisplayName = "Test api resource",
+                Enabled = true,
+                LastAccessed = DateTime.UtcNow,
+                Name = "Test api resource",
+                NonEditable = true,
+                Properties = new List<PropertyContract>(),
+                ShowInDiscoveryDocument = true,
+                Scopes = new List<ScopeContract>(),
+                Secrets = new List<ApiResourceSecretsContract>(),
+                Updated = DateTime.UtcNow
+            };
+            Current = Default;
+        }
+
+        private ApiResourceContract Default;
+        private ApiResourceContract Current;
+
+        public ApiResourceContractBuilder With(Action<ApiResourceContract> action)
+        {
+            action(Current);
+            return this;
+        }
+
+        public ApiResourceContract Build()
+        {
+            var current = Current;
+            Current = Default;
+            return current;
+        }
+
+    }
+}
--- IdentityServerAddOn/UnitTests/ValidatorTests/ApiResourceValidatorTests.cs
+++ IdentityServerAddOn/UnitTests/ValidatorTests/ApiResourceValidatorTests.cs
@@ -1,10 +1,10 @@
-﻿using FluentAssertions;
-using FluentValidation;
+﻿using FluentValidation;
 using FluentValidation.TestHelper;
 using Ids.SimpleAdmin.Backend;
 using Ids.SimpleAdmin.Contracts;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using UnitTests.ContractBuilders;
 using Xunit;
 
 namespace UnitTests.ValidatorTests
@@ -13,23 +13,46 @@ namespace UnitTests.ValidatorTests
     {
         IServiceCollection Services { get; set; }
         IServiceProvider Provider { get; set; }
+        ApiResourceContractBuilder ContractBuilder { get; set; }
         public ApiResourceValidatorTests()
         {
+            ContractBuilder = new ApiResourceContractBuilder();
             Services = new ServiceCollection();
             Services.AddSimpleAdminBackend();
             Provider = Services.BuildServiceProvider();
         }
         [Fact]
-        public void Hallo_world()
+        public void Test_Rules_For_Name_Property()
         {
             var validator = Provider.GetRequiredService<IValidator<ApiResourceContract>>();
-            var result = validator.TestValidate(new ApiResourceContract());
 
-            //should NOT have error
+            // Assert that there should NOT be a failure for the Name property.
+            var okName = new string('a', new Random().Next(2, 199));
+            var contract_ok = ContractBuilder.With(x => x.Name = okName).Build();
+            var result = validator.TestValidate(contract_ok);
             result.ShouldNotHaveValidationErrorFor(x => x.Name);
 
+            var contract_ok_min = ContractBuilder.With(x => x.Name = okName).Build();
+            result = validator.TestValidate(contract_ok_min);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+
+            var maxName = new string('a', 200);
+            var contract_ok_max = ContractBuilder.With(x => x.Name = okName).Build();
+            result = validator.TestValidate(contract_ok_max);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
 
             // Assert that there should be a failure for the Name property.
+            var contract_short = ContractBuilder.With(x => x.Name = string.Empty).Build();
+            result = validator.TestValidate(contract_short);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+
+            var longName = new string('a', 201);
+            var contract_long = ContractBuilder.With(x => x.Name = longName).Build();
+            result = validator.TestValidate(contract_long);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+
+            var contract_null = ContractBuilder.With(x => x.Name = null).Build();
+            result = validator.TestValidate(contract_null);
             result.ShouldHaveValidationErrorFor(x => x.Name);
         }
     }
