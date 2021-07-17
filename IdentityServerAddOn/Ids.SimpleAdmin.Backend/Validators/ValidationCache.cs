baa220d4f61239cf4c1f77ab5514054a02b82206
Stefan Petersen <stefangp88@gmail.com>
2021-07-17 12:19:43 +0200

    roles base settings error msg

--- IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/ValidationCache.cs
+++ IdentityServerAddOn/Ids.SimpleAdmin.Backend/Validators/ValidationCache.cs
@@ -145,6 +145,17 @@ namespace Ids.SimpleAdmin.Backend.Validators
             _summary["BaseSettings"] = CreateSummary(c, p);
             return _summary["BaseSettings"];
         }
+        public ErrorSummary BaseSettingSummary(RolesContract c)
+        {
+            if (_summary.ContainsKey("BaseSettings")) return _summary["BaseSettings"];
+            var p = new[]
+            {
+                nameof(c.Name),
+                nameof(c.NormalizedName)
+            };
+            _summary["BaseSettings"] = CreateSummary(c, p);
+            return _summary["BaseSettings"];
+        }
         public ErrorSummary LifeTimeSummary(ClientsContract c)
         {
             if (_summary.ContainsKey("LifeTime")) return _summary["LifeTime"];
