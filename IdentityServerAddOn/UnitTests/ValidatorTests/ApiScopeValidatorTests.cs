48a1c07e520638c160a5bd7ea7a672f7c278edbe
Stefan Petersen <stefangp88@gmail.com>
2021-07-19 19:43:02 +0200

    api scope validator tests

--- IdentityServerAddOn/UnitTests/ValidatorTests/ApiScopeValidatorTests.cs
+++ IdentityServerAddOn/UnitTests/ValidatorTests/ApiScopeValidatorTests.cs
@@ -51,15 +51,75 @@ namespace UnitTests.ValidatorTests
             result = validator.TestValidate(contract_null);
             result.ShouldNotHaveValidationErrorFor(x => x.Description);
 
-            var contract_short = ContractBuilder.With(x => x.Description = string.Empty).Build();
-            result = validator.TestValidate(contract_short);
-            result.ShouldNotHaveValidationErrorFor(x => x.Description);
-
             // Assert that there should be a failure for the Description property.
             var longDescription = new string('a', 1001);
             var contract_long = ContractBuilder.With(x => x.Description = longDescription).Build();
             result = validator.TestValidate(contract_long);
             result.ShouldHaveValidationErrorFor(x => x.Description);
+        }
+
+        [Fact]
+        public void Test_Rules_For_Name_Property()
+        {
+            var validator = Provider.GetRequiredService<IValidator<ApiScopeContract>>();
+
+            // Assert that there should NOT be a failure for the Name property.
+            var okName = new string('a', new Random().Next(1, 199));
+            var contract_ok = ContractBuilder.With(x => x.Name = okName).Build();
+            var result = validator.TestValidate(contract_ok);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+
+            var contract_ok_min = ContractBuilder.With(x => x.Name = string.Empty).Build();
+            result = validator.TestValidate(contract_ok_min);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+
+            var maxName = new string('a', 200);
+            var contract_ok_max = ContractBuilder.With(x => x.Name = maxName).Build();
+            result = validator.TestValidate(contract_ok_max);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+
+            // Assert that there should be a failure for the Name property.
+            var contract_null = ContractBuilder.With(x => x.Name = null).Build();
+            result = validator.TestValidate(contract_null);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+
+
+            var longName = new string('a', 201);
+            var contract_long = ContractBuilder.With(x => x.Name = longName).Build();
+            result = validator.TestValidate(contract_long);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+
+        }
+
+        [Fact]
+        public void Test_Rules_For_DisplayName_Property()
+        {
+            var validator = Provider.GetRequiredService<IValidator<ApiScopeContract>>();
+
+            // Assert that there should NOT be a failure for the DisplayName property.
+            var okDisplayName = new string('a', new Random().Next(1, 199));
+            var contract_ok = ContractBuilder.With(x => x.DisplayName = okDisplayName).Build();
+            var result = validator.TestValidate(contract_ok);
+            result.ShouldNotHaveValidationErrorFor(x => x.DisplayName);
+
+            var contract_ok_min = ContractBuilder.With(x => x.DisplayName = string.Empty).Build();
+            result = validator.TestValidate(contract_ok_min);
+            result.ShouldNotHaveValidationErrorFor(x => x.DisplayName);
+
+            var maxDisplayName = new string('a', 200);
+            var contract_ok_max = ContractBuilder.With(x => x.DisplayName = maxDisplayName).Build();
+            result = validator.TestValidate(contract_ok_max);
+            result.ShouldNotHaveValidationErrorFor(x => x.DisplayName);
+
+            var contract_null = ContractBuilder.With(x => x.DisplayName = null).Build();
+            result = validator.TestValidate(contract_null);
+            result.ShouldNotHaveValidationErrorFor(x => x.DisplayName);
+
+            // Assert that there should be a failure for the DisplayName property.
+            var longDisplayName = new string('a', 201);
+            var contract_long = ContractBuilder.With(x => x.DisplayName = longDisplayName).Build();
+            result = validator.TestValidate(contract_long);
+            result.ShouldHaveValidationErrorFor(x => x.DisplayName);
 
         }
     }
